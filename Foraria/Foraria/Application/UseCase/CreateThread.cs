74343bf6f390f145660849c957c0d881ca319a51
Santy <pizarro.santy@gmail.com>
2025-09-29 23:34:03 -0300

    Crear Hilo

---
+++ Foraria/Foraria/Application/UseCase/CreateThread.cs
@@ -0,0 +1,26 @@
+﻿using Foraria.Domain.Repository;
+using ForariaDomain;
+using Thread = ForariaDomain.Thread; //la entidad Thread choca con System.Threading.Thread
+
+
+namespace Foraria.Application.UseCase
+{
+    public class CreateThread
+    {
+        private readonly IThreadRepository _repository;
+
+        public CreateThread(IThreadRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<Thread> Execute(Thread thread)
+        {
+            thread.CreatedAt = DateTime.UtcNow;
+            thread.State = "Active";
+
+            await _repository.Add(thread);
+            return thread;
+        }
+    }
+}
---
+++ Foraria/Foraria/Interface/Controllers/ThreadController.cs
@@ -0,0 +1,41 @@
+﻿using Foraria.Application.UseCase;
+using Foraria.Domain.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Thread = ForariaDomain.Thread; //la entidad Thread choca con System.Threading.Thread 
+
+namespace Foraria.Interface.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ThreadsController : ControllerBase
+    {
+        private readonly CreateThread _createThread;
+        private readonly IThreadRepository _repository;
+
+        public ThreadsController(CreateThread createThread, IThreadRepository repository)
+        {
+            _createThread = createThread;
+            _repository = repository;
+        }
+
+        //nuevo hilo
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] Thread thread)
+        {
+            var createdThread = await _createThread.Execute(thread);
+            return CreatedAtAction(nameof(GetById), new { id = createdThread.Id }, createdThread);
+        }
+
+        //get hilo por id
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var thread = await _repository.GetById(id);
+            if (thread == null) return NotFound();
+            return Ok(thread);
+        }
+    }
+}
+
